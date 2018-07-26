065ef69f636d423cfb7dbf60b3d1c203cd413474
mottox2 <y.mottox2@gmail.com>
2018-07-26 11:08:40 +0900

    Refactor client

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,26 @@ interface Post {
   name: string
 }
 
+type Client = AxiosInstance
+
 class Teams {
-  async posts() {
-    return await axios.get('/posts')
+  client: Client
+
+  constructor(client: Client) {
+    this.client = client
+  }
+
+  async post(postId: string) {
+    return this.client.request<Post>({
+      method: 'get',
+      url: '/'
+    })
   }
 }
 
-class Client {
-  private client: AxiosInstance
+class Esa {
+  private client: Client;
+  private teams: Teams
 
   constructor(accessToken: string, teamName: string) {
     const baseUrl = `https://api.esa.io/v1/teams/${teamName}`
@@ -23,22 +35,9 @@ class Client {
       config.headers.Authorizaion = `Bearer ${accessToken}`
       return config
     })
+
+    this.teams = new Teams(this.client)
   }
-
-  get(path: string): Promise<any> {
-    return this.client.get(path)
-  }
-}
-
-class Esa {
-  private client: Client;
-  private teams: Teams
-
-  constructor(accessToken: string, teamName: string) {
-    this.client = new Client(accessToken, teamName)
-    this.teams = new Teams()
-  }
-
 }
 
 export default Esa
