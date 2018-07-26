657be37cc9bfe9ebd9348d7d24824c421a8820bf
mottox2 <y.mottox2@gmail.com>
2018-07-26 10:59:31 +0900

    Add client

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-console.log('Hello World')
+import axios, { AxiosInstance } from 'axios'
+
+interface Post {
+  name: string
+}
+
+class Teams {
+  async posts() {
+    return await axios.get('/posts')
+  }
+}
+
+class Client {
+  private client: AxiosInstance
+
+  constructor(accessToken: string, teamName: string) {
+    const baseUrl = `https://api.esa.io/v1/teams/${teamName}`
+    this.client = axios.create({
+      baseURL: baseUrl
+    })
+    
+    this.client.interceptors.request.use((config) => {
+      config.headers.Authorizaion = `Bearer ${accessToken}`
+      return config
+    })
+  }
+
+  get(path: string): Promise<any> {
+    return this.client.get(path)
+  }
+}
+
+class Esa {
+  private client: Client;
+  private teams: Teams
+
+  constructor(accessToken: string, teamName: string) {
+    this.client = new Client(accessToken, teamName)
+    this.teams = new Teams()
+  }
+
+}
+
+export default Esa
